7541c89080a496c9b3c79e6da69b2ab0637f35e7
+branden <+branden_ott@outlook.com+>
2021-11-02 14:51:10 -0400

    commit tree c4aad66e478ceaefad2b06307adad354717645d7

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(f){
   // Write your code here!
+    if (f <= 400) {
+      return 'This one is on me!'
+    } else if(f > 2500) {
+      return 'No can do.'
+    } else if (f > 2000 ) {
+      return 'I will gladly take your thirty bucks.'
+
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(isCity){
   // Write your code here!
+  return (isCity === 'NYC'? 'Ok, sounds good.' : 'No go.')
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (true) {
+    case tip === 'generous':
+      return 'Thank you so much.';
+    case tip === 'not as generous':
+      return 'Thank you.';
+    case tip === 'thanks for everything':
+      return 'Bye.';
+
+      
+  }
 }
